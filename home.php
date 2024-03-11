d2ea96158a89c855bce1c134920e394d61f21b7e
Sarahsegla <sarahsegla@icloud.com>
2024-03-11 20:08:57 +0000

    css not working. added font

---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <!-- swiper css -->
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+/>
+    <!-- font link -->
+    
+<link rel= "stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+<!-- styles -->
+<link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <!-- header -->
+    <section class="header">
+        <a href="home.php" class="logo">travel.</a>
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+</nav>
+
+</section>
+    <!-- header end-->
+
+
+
+<!-- swiper -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="js/script.js" >
+    </script>
+</body>
+</html>
